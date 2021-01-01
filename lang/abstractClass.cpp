1dc5c6a11bbf8eec3d091fe7c9fc496fb5b495e2
Bhaskar Tallamraju <bhaskar.tallamraju@gmail.com>
2021-01-01 20:40:57 +0800

    updated the abstract class and added override file

--- lang/abstractClass.cpp
+++ lang/abstractClass.cpp
@@ -4,7 +4,7 @@
 
 * Creation Date : 16-10-2020
 
-* Last Modified : Friday 16 October 2020 12:05:58 PM
+* Last Modified : Friday 01 January 2021 08:39:11 PM
 
 * Created By :  Bhaskar Tallamraju
 
@@ -17,41 +17,59 @@
 
 using namespace std;
 
+// Abstract class, as this forces the derived classes to have an implementation
 class Food
 {
 public:
-    virtual string getName() { return "Food"; }
+    Food() { cout << " Food Constructor called " << endl; }
+    // always good to have virtual destructor in base class to avoid any
+    // potential memory leaks, watch cherno virtual destructor
+    virtual ~Food() { cout << " Food Destructor called " << endl; }
+    virtual string type() = 0;
+    virtual int price() = 0;
+};
+
+class Spicy : public Food
+{
+private:
+    int m_price;
+    string m_name;
+public:
+    // constructor
+    Spicy(const string &name, int price)
+    : m_price(price), m_name(name) { cout << " Spicy Constructor called " << endl; }
+    ~Spicy() { cout << " Spicy Destructor called " << endl; }
+
+    string type() override { return "I am Spicy Food"; }
+    int price() override { return m_price; }
 };
 
 class Sweets : public Food
 {
 private:
-    string m_Name;
+    int m_price;
+    string m_name;
 public:
     // constructor
-    Sweets(const string& name) 
-        : m_Name(name) {}
+    Sweets(const string &name, int price)
+    : m_price(price), m_name(name) { cout << " Sweets Constructor called " << endl; }
+    ~Sweets() { cout << " Sweets Destructor called " << endl; }
 
-    // virtual function override
-    string getName() override { return m_Name; }
+    string type() override { return "I am Spicy Food"; }
+    int price() override { return m_price; }
 };
-
-void Print(Food* v)
-{
-    cout << v->getName() << endl;
-}
 
 int main (int argc, char *argv[])
 {
-    Food *f = new Food();
-    Print(f);
+    Sweets *sw = new Sweets("Desserts", 100);
+    Spicy *sp = new Spicy("Hot", 200);
 
-    Sweets *s = new Sweets("Desserts");
-    Print(s);
+    cout << "Type is " << sw->type() << endl;
+    cout << "Price is " << sw->price() << endl;
+    cout << "Type is " << sp->type() << endl;
+    cout << "Price is " << sp->price() << endl;
 
-    Food *f2 = s;
-    // without virtual in base class and override in derived, this would print Food, instead of Dessert
-    Print(f2);
-
+    delete sw;
+    delete sp;
 }
 
